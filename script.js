6b6e64134c8b0ed9728c8c5a710573e24f0d80ce
lorenasiombalo4 <168449589+lorenaiombalo4@users.noreply.github.com>
2025-05-28 10:23:15 -0300

    ok

--- script.js
+++ script.js
@@ -3,14 +3,23 @@ const caixaPrincipal = document.querySelector(".caixa-perguntas");
 const caixaPrincipal = document.querySelector(".caixa-alternativas");
 const caixaPrincipal = document.querySelector(".caixa-resultado");
 const caixaPrincipal = document.querySelector(".texto-resultado");
-
 const perguntas = [
 {
-    enunciado: "Pergunta1",
-    alternativas:["alternativa1","alternativa2"]
+    enunciado: "Pergunta1"
+    alternativas: [
+        {
+            texto: "alternativa1",
+            afirmacao:"afirmacao1"
+        },
+         {
+            texto: "alternativa2",
+            afirmacao:"afirmacao2"
+        }
+    ],
+        
 },
 {
-    enunciado: "Pergunta1"
+    enunciado: "Pergunta2"
     alternativas: ["alternativa1","alternativa2"]
+
 },
-]
